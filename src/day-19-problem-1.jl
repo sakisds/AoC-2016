357cc04b7d1dd5048b6a6133a41fd74847e50985
Thanasis Georgiou <contact@thgeorgiou.com>
2016-12-19 10:33:19 +0200

    Day 19 - WIP

---
+++ src/day-19-problem-1.jl
@@ -0,0 +1,38 @@
+### Day 19 - Problem 1
+## An Elephant Named Joseph
+## Author: Thanasis Georgiou
+workspace()
+
+# Input
+elfCount = 3014387
+
+# List item
+type ListItem
+  number::Int
+  next::Int
+end
+
+# Create list
+elves = map(i -> ListItem(i, i + 1), collect(1:elfCount))
+elves[end].next = 1
+
+# Find elfs
+i = 1 # Start from beginning
+while true
+  # Delete next elf
+  elf = elves[i]
+  elf.next = elves[elf.next].next
+
+  # Break if we are left with only one elf
+  if elf.number == elves[elf.next].number
+    break
+  end
+
+  i += 1
+  if i > elfCount
+    i = 1
+  end
+end
+
+# Elf number
+println("Last elf: $i")
